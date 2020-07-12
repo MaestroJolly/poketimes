998e65c1887a57b36ea804dcf80f4e243c55af9b
Yusuf Jolaoso <jolaosoyusuf@gmail.com>
2020-07-12 13:53:13 +0100

    Using route parameters to get id

--- src/hoc/Rainbow.js
+++ src/hoc/Rainbow.js
@@ -5,7 +5,7 @@ const Rainbow = (WrappedComponent) => {
     const colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellow', 'purple'];
     const randomColour = colours[Math.floor(Math.random() * 7)];
     const className = `${randomColour}-text`;
-    console.log(className);
+    // console.log(className);
 
     return (props) => {
         return (
