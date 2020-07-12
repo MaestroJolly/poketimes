1bede46f781439688e278e9d5684a235983c59ca
Yusuf Jolaoso <jolaosoyusuf@gmail.com>
2020-07-12 12:29:32 +0100

    Update: higher order component with Random component

---
+++ src/hoc/Rainbow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Rainbow = (WrappedComponent) => {
+
+    const colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellow', 'purple'];
+    const randomColour = colours[Math.floor(Math.random() * 7)];
+    const className = `${randomColour}-text`;
+    console.log(className);
+
+    return (props) => {
+        return (
+            <div className={ className }>
+                <WrappedComponent {...props} />
+            </div>
+        )
+    }
+}
+
+
+export default Rainbow;
